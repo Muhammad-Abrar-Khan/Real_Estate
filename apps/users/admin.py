f9415164de0e2da14088c4853ce13c70f723a00c
Muhammad-Abrar-Khan <makabrarkhan110@gmail.com>
2024-10-15 16:10:59 +0500

    created few models

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ['pkid','id','email','username','first_name','last_name','is_staff','is_active']
+    list_display_links = ['id','email']
+    list_filter = ['email','username','first_name','last_name','is_staff','is_active']
+    fieldsets = (
+        (
+            _("Login Credentials"), {}
+        )
+    )
